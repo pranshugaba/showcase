62f1db41e8547d3d6701ad7ca20e91ef5906a6a9
Pranshu Gaba <pranshugaba@gmail.com>
2022-11-04 21:45:22 +0530

    Add privacy policy

---
+++ pages/rng/index.tsx
@@ -0,0 +1,10 @@
+import Link from 'next/link';
+
+export default function Rng() {
+  return (
+    <>
+      <h1>Random Number Generator</h1>
+      <Link href='/rng/privacy'>Privacy policy</Link>
+    </>
+  );
+}
---
+++ pages/rng/privacy.tsx
@@ -0,0 +1,35 @@
+export default function Privacy() {
+  return (
+    <>
+      <main>
+        <h1 id='privacy-policy'>Privacy Policy</h1>
+        <h2 id='collection-of-personal-information'>
+          Collection of Personal Information
+        </h2>
+        <p>
+          We do not collect, store, use or share any information, personal or
+          otherwise.
+        </p>
+        <h2 id='email'>Email</h2>
+        <p>
+          If you email the developer for support or other feedback, the emails
+          with email addresses will be retained for quality assurance purposes.
+          The email addresses will be used only to reply to the concerns or
+          suggestions raised and will never be used for any marketing purpose.
+        </p>
+        <h2 id='disclosure-of-personal-information'>
+          Disclosure of Personal Information
+        </h2>
+        <p>
+          We will not disclose your information to any third party except if you
+          expressly consent or where required by law.
+        </p>
+        <h2 id='contacting-us'>Contacting Us</h2>
+        <p>
+          If you have any questions regarding this privacy policy, you can email
+          to <a href='mailto:hello@tenhourstudios.com'>rng+privacy@pranshugaba.com</a>.
+        </p>
+      </main>
+    </>
+  );
+}
